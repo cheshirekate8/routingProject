a7faee846f96bc73e4c2c334cdc4166f9b6322f3
Katie Young <Katie.f.Young@me.com>
2021-05-18 19:08:58 -0400

    Phase 5!

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get(/\/.*(xyz)/, (req, res) => {
 
 app.get("/capital-letters/:sub_dir", (req, res) => {
 
-    // const something = /capital-letters(.+\/)/ 
+    // const something = /capital-letters(.+\/)/
     // console.log(req.path)
     // console.log(something)
     res.send(req.params.sub_dir.toUpperCase())
@@ -33,3 +33,28 @@ app.all('*', function (req, res) {
 const port = 8081;
 
 app.listen(port, console.log(`Listenin' on port ${port}...`));
+
+//PHASE 5 STUFF
+// app.get('/margot/:letters', (req, res) => {
+//     const firstLetter = req.params.letters[0].toUpperCase();
+//     res.send(firstLetter + req.params.letters.slice(1));
+//  })
+
+//  app.get('/margeaux/:letters', (req, res) => {
+//     const firstLetter = req.params.letters[0].toUpperCase();
+//     res.send(firstLetter + req.params.letters.slice(1));
+//  })
+
+// app.get('/about/foo', (req, res) => {
+//     res.send(404);
+//  });
+
+//  app.all(/^\/[A-Za-z0-9\-_]*$/, (req, res) => {
+//     //res.send("Something went wrong.");
+//     const pageData = {
+//        method: req.method,
+//        path: req.path,
+//        randomNumber: 5
+//     };
+//     res.render("layout", pageData);
+//  });

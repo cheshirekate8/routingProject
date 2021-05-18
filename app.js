7ae2883e357d8d58aa69536df02ec844feafdad5
Joubin T <jubin.taghdir@gmail.com>
2021-05-18 14:31:02 -0700

    phase 2

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
+app.all('*', function (req, res) {
+    res.render('layout', {
+        //assign variables here
+        randomNumber: 2,
+        path: req.path,
+        method: req.method
+    })
+})
+
 app.get('/', function (req, res) {
     res.send('Hello from Express!')
 });
